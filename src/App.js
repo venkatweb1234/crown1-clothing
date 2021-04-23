594da0da403b4a7df30ee578f94f24d5c10060a7
Venkatesh Venkatesh <venkatesh.venkatesh@optus.com.au>
2021-04-23 16:30:10 +1000

    first commit

---
+++ src/App.js
@@ -0,0 +1,13 @@
+import './App.css';
+
+import HomePage from'./components/Homepage'
+
+function App() {
+  return (
+    <div>
+      <HomePage></HomePage>
+    </div>
+  );
+}
+
+export default App;
---
+++ src/components/Homepage.jsx
@@ -0,0 +1,38 @@
+import React from 'react';
+import './Homepage.style.scss'
+const HomePage = () => {
+    return (<div>
+        <div className="menu-item">
+            <div className="content">
+                <h1 className="title">HATS</h1>
+                <span className="subtitle">SHOP NOW</span>
+            </div>
+        </div>
+        <div className="menu-item">
+            <div className="content">
+                <h1 className="title">JACKETS</h1>
+                <span className="subtitle">SHOP NOW</span>
+            </div>
+        </div>
+        <div className="menu-item">
+            <div className="content">
+                <h1 className="title">SNEAKERS</h1>
+                <span className="subtitle">SHOP NOW</span>
+            </div>
+        </div>
+        <div className="menu-item">
+            <div className="content">
+                <h1 className="title">WOMENS</h1>
+                <span className="subtitle">SHOP NOW</span>
+            </div>
+        </div>
+        <div className="menu-item">
+            <div className="content">
+                <h1 className="title">MENS</h1>
+                <span className="subtitle">SHOP NOW</span>
+            </div>
+        </div>
+    </div>)
+}
+
+export default HomePage;
